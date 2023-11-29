da1e98006477248c7e6068e54b38d7ba301d64b4
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-11-29 10:47:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -19,18 +19,20 @@ window.onload = () => {
     let scene = document.querySelector('a-scene');
     let lat = JSON.parse(sessionStorage.getItem('lat'));
     let lon = JSON.parse(sessionStorage.getItem('lon'));
-    console.log(lat);
-    console.log(lon);
+    // console.log(lat);
+    // console.log(lon);
   
     for (let i = 0; i < lon.length; i++) {
         let latitude = lat[i];
         let longitude = lon[i];
-        let model = document.createElement('a-box');
+        console.log(latitude);
+        console.log(longitude);
+        let model = document.createElement('a-entity');
         model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('material', { color: 'blue' } );
-        // model.setAttribute('look-at', '[gps-new-camera]')
-        // model.setAttribute('gltf-model', '#animated-asset');
-        // model.setAttribute('animation-mixer', 'loop: repeat');
+        // model.setAttribute('material', { color: 'blue' } );
+        model.setAttribute('look-at', '[gps-new-camera]')
+        model.setAttribute('gltf-model', '#animated-asset');
+        model.setAttribute('animation-mixer', 'loop: repeat');
         model.setAttribute('scale', '0.6633601288757837 0.6633601288757837 0.6633601288757837');
 
         model.addEventListener('loaded', () => {
