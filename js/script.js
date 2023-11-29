e52b526fe33672a96c1213885e18b381af02f1f2
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-11-29 10:04:23 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,42 @@
 window.onload = () => {
-    let places = staticLoadPlaces();
-    renderPlaces(places);
+    // let places = staticLoadPlaces();
+    renderPlaces();
 };
 
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Magnemite',
-            location: {
-                lat: -7.2892116,
-                lng: 112.7969294,
-            }
-        },
-    ];
-  }
+// function staticLoadPlaces() {
+//     return [
+//         {
+//             name: 'Magnemite',
+//             location: {
+//                 lat: -7.2892116,
+//                 lng: 112.7969294,
+//             }
+//         },
+//     ];
+//   }
   
-  function renderPlaces(places) {
+  function renderPlaces() {
     let scene = document.querySelector('a-scene');
+    let lat = JSON.parse(sessionStorage.getItem('lat'));
+    let lon = JSON.parse(sessionStorage.getItem('lon'));
+    console.log(lat);
+    console.log(lon);
   
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-  
-        let model = document.createElement('a-box');
+    for (let i = 0; i < lon.length; i++) {
+        let latitude = lat[i];
+        let longitude = lon[i];
+        let model = document.createElement('a-entity');
         model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        model.setAttribute('material', { color: 'blue' } );
-        model.setAttribute('scale', '0.5 0.5 0.5');
-  
+        // model.setAttribute('material', { color: 'blue' } );
+        model.setAttribute('look-at', '[gps-new-camera]')
+        model.setAttribute('gltf-model', '#animated-asset');
+        model.setAttribute('animation-mixer', 'loop: repeat');
+        model.setAttribute('scale', '0.6633601288757837 0.6633601288757837 0.6633601288757837');
+
         model.addEventListener('loaded', () => {
             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
         });
-  
         scene.appendChild(model);
-    });
+    }
   }
   
