11609d9639c98a2881b3dcb23de5719ede0b8507
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-11-30 21:03:19 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,6 @@
 window.onload = () => {
-    // let places = staticLoadPlaces();
     renderPlaces();
 };
-
-// function staticLoadPlaces() {
-//     return [
-//         {
-//             name: 'Magnemite',
-//             location: {
-//                 lat: -7.2892116,
-//                 lng: 112.7969294,
-//             }
-//         },
-//     ];
-//   }
   
   function renderPlaces() {
     let scene = document.querySelector('a-scene');
@@ -22,7 +9,7 @@ window.onload = () => {
     // console.log(lat);
     // console.log(lon);
   
-    for (let i = 0; i < lat.length; i++) {
+    for (let i = lat.length-1; i == 0; i--) {
         let latitude = lat[i];
         let longitude = lon[i];
         console.log(latitude);
@@ -30,7 +17,11 @@ window.onload = () => {
         let model = document.createElement('a-entity');
         model.setAttribute('gps-new-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
         // model.setAttribute('material', { color: 'blue' } );
-        model.setAttribute('look-at', '[gps-new-camera]')
+        // model.setAttribute('look-at', '[gps-new-camera]');
+        model.setAttribute('id', 'target'+i);
+        if (i != lat.length-1) {
+            model.setAttribute('look-at', '#target'+(i+1));
+        } 
         model.setAttribute('gltf-model', '#animated-asset');
         model.setAttribute('animation-mixer', 'loop: repeat');
         model.setAttribute('scale', '0.6633601288757837 0.6633601288757837 0.6633601288757837');
