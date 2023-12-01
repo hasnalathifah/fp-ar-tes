1ed08eccc2a98831da839dbab8d3f580f9f4726c
hasnalathifah <91044599+hasnalathifah@users.noreply.github.com>
2023-12-01 20:35:58 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -32,12 +32,12 @@ window.onload = () => {
         // model.setAttribute('material', { color: 'blue' } );
         model.setAttribute('look-at', '[gps-new-camera]');
         let id = 'target'+i;
-        let target = '#target'+(i+1);
-        console.log(id);
-        console.log(target);
+        let target = '#target'+(i-1);
         model.setAttribute('id', id);
-        if (i != lat.length-1) {
+        if (i != 0) {
             model.setAttribute('look-at', target);
+            console.log(id);
+            console.log(target);
         } 
         model.setAttribute('gltf-model', '#animated-asset');
         model.setAttribute('animation-mixer', 'loop: repeat');
